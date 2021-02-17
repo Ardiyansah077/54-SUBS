34cc79ad2cd7c27af04ae08008d6e317f0ad8d43
Ardiyansah077 <78828300+Ardiyansah077@users.noreply.github.com>
2021-02-17 15:54:38 +0700

    Add files via upload

---
+++ settings/apikey.js
@@ -0,0 +1,4 @@
+const apikey = {
+        BarBarKey: 'IDxO1TFYnKADlX4pxcHa',
+        TobzKey: 'BotWeA',
+}
---
+++ settings/config.js
@@ -0,0 +1,6 @@
+const config = {
+        botName: 'PATRICKBOT',
+        ownerName: 'OWNER PATRICK❤️😆',
+        youtube: 'https://youtube.com/channel/UC2Z3E4wBSKXNZBGIbkGNnsw',
+        instagram: 'INSTAGRAM_LINK',
+}
