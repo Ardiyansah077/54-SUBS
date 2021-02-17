d4a61b68ded990dadfd4758c46b699a57cc50e18
Ardiyansah077 <78828300+Ardiyansah077@users.noreply.github.com>
2021-02-17 15:55:46 +0700

    Add files via upload

---
+++ src/daftarvip.js
@@ -0,0 +1,10 @@
+const daftarvip = (prefix, pushname) => {
+	return ``\`\`\`JANGAN LUPA BERNAFAS 🐦😆🤭`\`\`\`
+``\`\`\`MAU DAFTAR VIP = OWNER/PREMIUM``\`\`\`
+``\`\`\`👇SILAHKAN HUBUNGI SAYA🤗``\`\`\`
+wa : https://wa.me/6285282996146
+``\`\`\`HUBUNGI TUH JANGAN LUPA SALAM JIKA``\`\`\`
+``\`\`\`ANDA ISLAM, NAMANYA ARDIYANSAH YA...``\`\`\`
+}
+
+exports.daftarvip = daftarvip
---
+++ src/help.js
@@ -0,0 +1,215 @@
+const help = (prefix) => {
+	return `
+「 *PATRICK BOT* 」
+
+◪ *info OWNER*
+  ❏ Prefix: 「  **${prefix}  」
+  ❏ Creator : ARDIGANS YT
+  ❏ YOUTUBE: https://youtube.com/channel/UC2Z3E4wBSKXNZBGIbkGNnsw
+          *IKLAN*
+`\`\`\`👇WHATSAPP OWNER😆`\`\`\`
+https://wa.me/6285282996146
+`\`\`\`👇JANGAN LUPA SUBSCRIBE YT PEMBUAT SC🤗`\`\`\`
+https://youtube.com/channel/UC2Z3E4wBSKXNZBGIbkGNnsw
+
+  ┏━⊱ *TENTANG BOT* ⊰━┓
+  ┣⊱❥ *${prefix}info*
+  ┣⊱❥ *${prefix}owner*
+  ┣⊱❥ *${prefix}blocklist*
+  ┣⊱❥ *${prefix}chatlist*
+  ┣⊱❥ *${prefix}ping*
+  ┣⊱❥ *${prefix}bug*
+  ┣⊱❥ *${prefix}DaftarVip*
+  ┗━━━━━━━━━━━━━━━━━
+
+  ┏━━⊱ *MAKER MENU* ⊰━━┓
+  ┣⊱❥ *${prefix}sticker*
+  ┣⊱❥ *${prefix}stickergif*
+  ┣⊱❥ *${prefix}shota*
+  ┣⊱❥ *${prefix}bpink*
+  ┣⊱❥ *${prefix}marvellogo*
+  ┣⊱❥ *${prefix}snowwrite*
+  ┣⊱❥ *${prefix}3dtext*
+  ┣⊱❥ *${prefix}ninjalogo*
+  ┣⊱❥ *${prefix}water*
+  ┣⊱❥ *${prefix}firetext*
+  ┣⊱❥ *${prefix}logowolf*
+  ┣⊱❥ *${prefix}logowolf2*
+  ┣⊱❥ *${prefix}phlogo*
+  ┣⊱❥ *${prefix}glitch*
+  ┣⊱❥ *${prefix}neonlogo*
+  ┣⊱❥ *${prefix}neonlogo2*
+  ┣⊱❥ *${prefix}lionlogo*
+  ┣⊱❥ *${prefix}jokerlogo*
+  ┣⊱❥ *${prefix}shadow*
+  ┣⊱❥ *${prefix}burnpaper*
+  ┣⊱❥ *${prefix}coffee*
+  ┣⊱❥ *${prefix}lovepaper*
+  ┣⊱❥ *${prefix}woodblock*
+  ┣⊱❥ *${prefix}qowheart*
+  ┣⊱❥ *${prefix}mutgrass*
+  ┣⊱❥ *${prefix}undergocean*
+  ┣⊱❥ *${prefix}woodenboards*
+  ┣⊱❥ *${prefix}wolfmetal*
+  ┣⊱❥ *${prefix}metalictglow*
+  ┣⊱❥ *${prefix}8bit*
+  ┣⊱❥ *${prefix}firetext*
+  ┣⊱❥ *${prefix}3dtext*
+  ┣⊱❥ *${prefix}ttp*
+  ┣⊱❥ *${prefix}herrypotter🤗*
+  ┣⊱❥ *${prefix}pubglogo*
+  ┣⊱❥ *${prefix}quotemaker*
+  ┗━━━━━━━━━━━━━━━━━━━
+ 
+  ┏━━⊱ *EDUCATION* ⊰━━┓
+  ┣⊱❥ *${prefix}trendtwit*
+  ┣⊱❥ *${prefix}randomkpop*
+  ┣⊱❥ *${prefix}ytsearch*
+  ┣⊱❥ *${prefix}wiki*
+  ┣⊱❥ *${prefix}wikien*
+  ┣⊱❥ *${prefix}nulis*
+  ┣⊱❥ *${prefix}quotes*
+  ┣⊱❥ *${prefix}quotes2*
+  ┣⊱❥ *${prefix}artinama*
+  ┗━━━━━━━━━━━━━━━━━
+  
+  ┏━━⊱ *KERANG MENU* ⊰━━┓
+  ┣⊱❥ *${prefix}apakah*
+  ┣⊱❥ *${prefix}kapankah*
+  ┣⊱❥ *${prefix}rate*
+  ┣⊱❥ *${prefix}game*
+  ┣⊱❥ *${prefix}bisakah*
+  ┗━━━━━━━━━━━━━━━━━━━
+  
+  ┏━━⊱ *DOWNLOADER* ⊰━━┓
+  ┣⊱❥ *${prefix}images*
+  ┣⊱❥ *${prefix}ytmp3*
+  ┣⊱❥ *${prefix}ytmp4*
+  ┣⊱❥ *${prefix}tiktok*
+  ┣⊱❥ *${prefix}joox*
+  ┣⊱❥ *${prefix}toimg*
+  ┣⊱❥ *${prefix}tomp3*
+  ┣⊱❥ *${prefix}meme*
+  ┣⊱❥ *${prefix}memeindo*
+  ┗━━━━━━━━━━━━━━━━━━
+
+  ┏━━⊱ *GABUT MENU* ⊰━━┓
+  ┣⊱❥ *${prefix}play*
+  ┣⊱❥ *${prefix}tts*
+  ┣⊱❥ *${prefix}lirik*
+  ┣⊱❥ *${prefix}chord*
+  ┣⊱❥ *${prefix}quran*
+  ┣⊱❥ *${prefix}tiktokstalk*
+  ┣⊱❥ *${prefix}trendtwit*
+  ┣⊱❥ *${prefix}igstalk*
+  ┣⊱❥ *${prefix}randombokep*
+  ┗━━━━━━━━━━━━━━━━━
+
+  ┏━━⊱ *WIBU MENU* ⊰━━┓
+  ┣⊱❥ *${prefix}neonime*
+  ┣⊱❥ *${prefix}pokemon*
+  ┣⊱❥ *${prefix}loli*
+  ┣⊱❥ *${prefix}waifu*
+  ┣⊱❥ *${prefix}randomanime*
+  ┣⊱❥ *${prefix}husbu*
+  ┣⊱❥ *${prefix}husbu2*
+  ┣⊱❥ *${prefix}animecry*
+  ┣⊱❥ *${prefix}wait*
+  ┣⊱❥ *${prefix}neko*
+  ┣⊱❥ *${prefix}nekonime*
+  ┗━━━━━━━━━━━━━━━━━
+
+  ┏━━⊱ *18++ MENU* ⊰━━┓
+  ┣⊱❥ *${prefix}randomhentai*
+  ┣⊱❥ *${prefix}nsfwtrap*
+  ┣⊱❥ *${prefix}nsfwloli*
+  ┣⊱❥ *${prefix}nsfwbobs*
+  ┣⊱❥ *${prefix}nsfwass*
+  ┣⊱❥ *${prefix}nsfwlowjob*
+  ┣⊱❥ *${prefix}nsfwneko*
+  ┣⊱❥ *${prefix}randombokep*
+  ┗━━━━━━━━━━━━━━━━━
+
+  ┏━━⊱ *SOSIAL MENU* ⊰━━┓
+  ┣⊱❥ *${prefix}alay*
+  ┣⊱❥ *${prefix}gantengcek*
+  ┣⊱❥ *${prefix}cantikcek*
+  ┣⊱❥ *${prefix}watak*
+  ┣⊱❥ *${prefix}hobby*
+  ┣⊱❥ *${prefix}game*
+  ┣⊱❥ *${prefix}bucin*
+  ┣⊱❥ *${prefix}trust*
+  ┣⊱❥ *${prefix}dare*
+  ┣⊱❥ *${prefix}map*
+  ┣⊱❥ *${prefix}hilih*
+  ┣⊱❥ *${prefix}simi*
+  ┗━━━━━━━━━━━━━━━━
+
+  ┏━━⊱ *INFORMASI* ⊰━━┓
+  ┣⊱❥ *${prefix}bahasa*
+  ┣⊱❥ *${prefix}kodenegara*
+  ┣⊱❥ *${prefix}kbbi*
+  ┣⊱❥ *${prefix}wiki*
+  ┣⊱❥ *${prefix}wikien*
+  ┣⊱❥ *${prefix}fakta*
+  ┣⊱❥ *${prefix}infocuaca*
+  ┣⊱❥ *${prefix}infogempa*
+  ┣⊱❥ *${prefix}jadwaltvnow*
+  ┣⊱❥ *${prefix}covid*
+  ┗━━━━━━━━━━━━━━━━━
+
+  ┏━━⊱ *ADMIN MENU* ⊰━━┓
+  ┣⊱❥ *${prefix}add*
+  ┣⊱❥ *${prefix}kick*
+  ┣⊱❥ *${prefix}groupbuka*
+  ┣⊱❥ *${prefix}grouptutup*
+  ┣⊱❥ *${prefix}tagme*
+  ┣⊱❥ *${prefix}tagall*
+  ┣⊱❥ *${prefix}tagall2*
+  ┣⊱❥ *${prefix}tagall3*
+  ┣⊱❥ *${prefix}tagall4*
+  ┣⊱❥ *${prefix}tagall5*
+  ┣⊱❥ *${prefix}linkgc*
+  ┣⊱❥ *${prefix}ownergrup*
+  ┣⊱❥ *${prefix}hidetag*
+  ┣⊱❥ *${prefix}setpp*
+  ┣⊱❥ *${prefix}setname*
+  ┣⊱❥ *${prefix}setdec*
+  ┣⊱❥ *${prefix}listadmin*
+  ┣⊱❥ *${prefix}simih [1/0]*
+  ┣⊱❥ *${prefix}welcome [1/0]*
+  ┣⊱❥ *${prefix}nsfw [1/0]*
+  ┗━━━━━━━━━━━━━━━━━━
+
+  ┏━━⊱ *MENU OWNER* ⊰━━┓
+  ┣⊱❥ *${prefix}setprefix*
+  ┣⊱❥ *${prefix}block*
+  ┣⊱❥ *${prefix}unblock*
+  ┣⊱❥ *${prefix}setppbot*
+  ┣⊱❥ *${prefix}blocklist*
+  ┣⊱❥ *${prefix}chatlist*
+  ┣⊱❥ *${prefix}delete*
+  ┣⊱❥ *${prefix}bc*
+  ┣⊱❥ *${prefix}bcgc*
+  ┣⊱❥ *${prefix}clone*
+  ┣⊱❥ *${prefix}clearall*
+  ┗━━━━━━━━━━━━━━━━━
+
+  ┏━━⊱ *OUTROS* ⊰━━┓
+  ┣⊱❥ *${prefix}send*
+  ┣⊱❥ *${prefix}spamcall*
+  ┣⊱❥ *${prefix}wame*
+  ┣⊱❥ *${prefix}virtex*
+  ┣⊱❥ *${prefix}exe*
+  ┣⊱❥ *${prefix}qrcode*
+  ┣⊱❥ *${prefix}afk*
+  ┣⊱❥ *${prefix}timer*
+  ┣⊱❥ *${prefix}testime*
+  ┣⊱❥ *${prefix}semoji*
+  ┣⊱❥ *${prefix}fml*
+  ┣⊱❥ *${prefix}fml2*
+  ┗━━━━━━━━━━━━━━━
+`
+}
+
+exports.help = help
